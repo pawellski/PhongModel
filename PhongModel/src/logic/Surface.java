a37aa5811a667309fc41db4bf6b59587d6c28497
Paweł <skibaapawell@gmail.com>
2020-05-30 15:10:21 +0200

    Dodanie wsploczynnika Ka oraz usuniecie setterow w klasie Surface

--- PhongModel/src/logic/Surface.java
+++ PhongModel/src/logic/Surface.java
@@ -2,39 +2,34 @@ package logic;
 
 public class Surface {
 
+    private double KA;
     private double KD;
     private double KS;
     private double N;
 
-    public Surface(double KD, double KS, double N) {
+    public Surface(double KA, double KD, double KS, double N) {
+        this.KA = KA;
         this.KD = KD;
         this.KS = KS;
         this.N = N;
+    }
+
+    public double getKA() {
+        return KA;
     }
 
     public double getKD() {
         return KD;
     }
 
-    public void setKD(double KD) {
-        this.KD = KD;
-    }
-
     public double getKS() {
         return KS;
-    }
-
-    public void setKS(double KS) {
-        this.KS = KS;
     }
 
     public double getN() {
         return N;
     }
 
-    public void setN(double N) {
-        this.N = N;
-    }
 
     @Override
     public String toString() {
