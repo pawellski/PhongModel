245fa6696e52e6c14ed46c06090548c4c6d2f3dd
Paweł <skibaapawell@gmail.com>
2020-05-30 15:12:24 +0200

    Dodanie sterowania swiatlem oraz roznych materialow (nieskonfigurowane)

--- PhongModel/src/controller/Controller.java
+++ PhongModel/src/controller/Controller.java
@@ -12,13 +12,16 @@ public class Controller {
 
     private GraphicsContext map;
     private Surface surface;
+    private Color color;
 
     @FXML
     private Canvas canvas;
 
     public void initialize() {
+        color = Color.SADDLEBROWN;
+        surface = new Surface(0.4, 0.75, 0.25, 5);
         prepareBackground();
-        prepareInitialPhong();
+        PhongShading.phongAlgoritm(map, surface);
     }
 
     @FXML
@@ -27,20 +30,44 @@ public class Controller {
         String key = k.getCode().toString();
         switch (key) {
             case "W":
-                System.out.println(key);
+                PhongShading.moveUp();
                 break;
             case "S":
-                System.out.println(key);
+                PhongShading.moveDown();
                 break;
             case "A":
-                System.out.println(key);
+                PhongShading.moveLeft();
                 break;
             case "D":
-                System.out.println(key);
+                PhongShading.moveRight();
+                break;
+            case "Z":
+                PhongShading.moveForward();
+                break;
+            case "X":
+                PhongShading.moveBackward();
+                break;
+            case "DIGIT1":
+                color = Color.SADDLEBROWN;
+                surface = new Surface(0.4, 0.75, 0.25, 5);
+                break;
+            case "DIGIT2":
+                color = Color.WHITE;
+                surface = new Surface(0.05, 0.5, 0.7, 0.078125);
+                break;
+            case "DIGIT3":
+                color = Color.SADDLEBROWN;
+                surface = new Surface(0.4, 0.25, 0.75, 100);
+                break;
+            case "DIGIT4":
+                color = Color.TURQUOISE;
+                surface = new Surface(0.4, 0.75, 0.25, 5);
                 break;
             default:
                 break;
         }
+        prepareBackground();
+        PhongShading.phongAlgoritm(map, surface);
     }
 
     public void focus() {
@@ -50,16 +77,10 @@ public class Controller {
     private void prepareBackground() {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setFill(Color.BLACK);
-        gc.fillRect(0, 0, 800, 800);
+        gc.fillRect(0, 0, 500, 500);
         map = canvas.getGraphicsContext2D();
-        map.setFill(Color.DARKORANGE);
-        map.fillOval(200, 200, 400, 400);
-        
-    }
-
-    private void prepareInitialPhong() {
-        surface = new Surface(0.25, 0.75, 25);
-        PhongShading.phongAlgoritm(map, surface);
+        map.setFill(color);
+        map.fillOval(100, 100, 300, 300);
     }
 
 }
